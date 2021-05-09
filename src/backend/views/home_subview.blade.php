89966611109679410f914dc85f42193ecdf92d22
zmcms <59611848+zmcms@users.noreply.github.com>
2021-05-09 22:47:38 +0200

    Add files via upload

--- src/backend/views/home_subview.blade.php
+++ src/backend/views/home_subview.blade.php
@@ -1,4 +1,4 @@
-<div class="home_module home_module micro12 small6 medium2">
+<div class="home_module micro12 small6 medium3">
     <h1><span class="fas fa-bars"></span> Oferta</h1>
     <button id="btn_zwol">Lista ofert</button>
     <button id="btn_zwopl">Produkty</button>
