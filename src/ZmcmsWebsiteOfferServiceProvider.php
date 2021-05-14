ef2ef59357ce9a0c63c9c3d5d94e76ee1de3c08f
zmcms <59611848+zmcms@users.noreply.github.com>
2021-05-14 12:40:08 +0200

    Update ZmcmsWebsiteOfferServiceProvider.php

--- src/ZmcmsWebsiteOfferServiceProvider.php
+++ src/ZmcmsWebsiteOfferServiceProvider.php
@@ -10,17 +10,14 @@ class ZmcmsWebsiteOfferServiceProvider extends ServiceProvider{
 	}
 
 	public function boot(){
-		// $this->loadRoutesFrom(__DIR__.DIRECTORY_SEPARATOR.'backend'.DIRECTORY_SEPARATOR.'routes'.DIRECTORY_SEPARATOR.'website_offer.php');
-		// $this->loadRoutesFrom(__DIR__.DIRECTORY_SEPARATOR.'backend'.DIRECTORY_SEPARATOR.'routes'.DIRECTORY_SEPARATOR.'website_offer_console.php');
-		// $this->loadRoutesFrom(__DIR__.DIRECTORY_SEPARATOR.'frontend'.DIRECTORY_SEPARATOR.'routes'.DIRECTORY_SEPARATOR.'website_offer.php');
 		$this->loadMigrationsFrom(__DIR__.'/migrations');
 		$this->publishes([
-			__DIR__.'/config' => base_path('config/'.Config('zmcms.frontend.theme_name')),
-			__DIR__.'/backend/css' => base_path('public/themes/'.Config('zmcms.frontend.theme_name').'/backend/css/'),
-			__DIR__.'/backend/js' => base_path('public/themes/'.Config('zmcms.frontend.theme_name').'/backend/js/'),
+			__DIR__.'/config' => base_path('config/'),
+			__DIR__.'/backend/css' => base_path('public/themes/backend/css/'),
+			__DIR__.'/backend/js' => base_path('public/themes/backend/js/'),
 			__DIR__.'/backend/views' => base_path('resources/views/themes/zmcms/backend'),
-			__DIR__.'/frontend/css' => base_path('public/themes/'.Config('zmcms.frontend.theme_name').'/frontend/css/'),
-			__DIR__.'/frontend/js' => base_path('public/themes/'.Config('zmcms.frontend.theme_name').'/frontend/js/'),
+			__DIR__.'/frontend/css' => base_path('public/themes/frontend/css/'),
+			__DIR__.'/frontend/js' => base_path('public/themes/frontend/js/'),
 			__DIR__.'/frontend/views' => base_path('resources/views/themes/zmcms/frontend'),
 		]);
 	}
