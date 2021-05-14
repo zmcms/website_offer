5d0e35f246451d6b38eb7474e060cd217a136138
zmcms <59611848+zmcms@users.noreply.github.com>
2021-05-14 12:44:39 +0200

    Update ZmcmsWebsiteOfferServiceProvider.php

--- src/ZmcmsWebsiteOfferServiceProvider.php
+++ src/ZmcmsWebsiteOfferServiceProvider.php
@@ -12,12 +12,12 @@ class ZmcmsWebsiteOfferServiceProvider extends ServiceProvider{
 	public function boot(){
 		$this->loadMigrationsFrom(__DIR__.'/migrations');
 		$this->publishes([
-			__DIR__.'/config' => base_path('config/'),
-			__DIR__.'/backend/css' => base_path('public/themes/backend/css/'),
-			__DIR__.'/backend/js' => base_path('public/themes/backend/js/'),
+			__DIR__.'/config' => base_path('config/zmcms'),
+			__DIR__.'/backend/css' => base_path('public/themes/zmcms/backend/css/'),
+			__DIR__.'/backend/js' => base_path('public/themes/zmcms/backend/js/'),
 			__DIR__.'/backend/views' => base_path('resources/views/themes/zmcms/backend'),
-			__DIR__.'/frontend/css' => base_path('public/themes/frontend/css/'),
-			__DIR__.'/frontend/js' => base_path('public/themes/frontend/js/'),
+			__DIR__.'/frontend/css' => base_path('public/themes/zmcms/frontend/css/'),
+			__DIR__.'/frontend/js' => base_path('public/themes/zmcms/frontend/js/'),
 			__DIR__.'/frontend/views' => base_path('resources/views/themes/zmcms/frontend'),
 		]);
 	}
